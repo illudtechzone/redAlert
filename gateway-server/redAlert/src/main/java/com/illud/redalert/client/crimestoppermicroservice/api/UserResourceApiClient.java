af24ed9b2d2f93bbffc52a9f1798e45e41a22864
Prince <prince>
2019-03-30 10:51:27 +0530

    location resource REST

--- gateway-server/redAlert/src/main/java/com/illud/redalert/client/crimestoppermicroservice/api/UserResourceApiClient.java
+++ gateway-server/redAlert/src/main/java/com/illud/redalert/client/crimestoppermicroservice/api/UserResourceApiClient.java
@@ -1,8 +1,8 @@
 package com.illud.redalert.client.crimestoppermicroservice.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.illud.redalert.client.crimestoppermicroservice.ClientConfiguration;
+import com.illud.redalert.client.crimestoppermicroservice.ClientConfigurationCS;
 
-@FeignClient(name="${crimestoppermicroservice.name:crimestoppermicroservice}", url="${crimestoppermicroservice.url:localhost:8084/crimestopper}", configuration = ClientConfiguration.class)
+@FeignClient(name="${crimestoppermicroservice.name:crimestoppermicroservice}", url="${crimestoppermicroservice.url:localhost:8084/crimestopper}", configuration = ClientConfigurationCS.class)
 public interface UserResourceApiClient extends UserResourceApi {
 }
