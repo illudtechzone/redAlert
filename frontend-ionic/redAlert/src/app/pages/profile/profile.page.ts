bd08496662eb20b11487ee1f9e1d4ec4bf890b46
imanzal <ansalkhan.k.a@lxisoft.com>
2019-03-30 09:22:27 +0530

    view user profile

--- frontend-ionic/redAlert/src/app/pages/profile/profile.page.ts
+++ frontend-ionic/redAlert/src/app/pages/profile/profile.page.ts
@@ -1,4 +1,7 @@
+import { CurrentUserService } from './../../security/current-user.service';
+import { UserResourceService } from 'src/app/shared/gateway-api/services';
 import { Component, OnInit } from '@angular/core';
+import { UserDTO } from 'src/app/shared/gateway-api/models';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  user: UserDTO = {
+  };
+  constructor(private userResource: UserResourceService, private  currentUser: CurrentUserService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    await this.currentUser.getCurrentUser(false).then( (usr) => {
+      this.user = usr;
+    });
   }
 
 }
